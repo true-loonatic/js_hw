ace3884ba98b9fc001d4f20342f861567777e9ce
true-loonatic <azinland@gmail.com>
2022-06-15 20:48:19 +0300

    js homework start

---
+++ js_hw.js
@@ -0,0 +1,22 @@
+// let apples = 10;
+// console.log("apples =", apples);
+
+let item_1 = 5;
+console.log("item 1 =", item_1);
+let item_2 = 3;
+console.log("item 2 =", item_2);
+let item_3 = item_1 + item_2;
+console.log("item 3 =", item_3);
+let item_4 = "Yolochka";
+console.log("item 4 =", item_4);
+
+console.log(item_3 + item_4);
+console.log(item_3 * item_4);
+
+let item_5 = item_3;
+let item_6 = 15;
+let item_6_type = typeof (item_6);
+
+let.t
+// if (typeof item_6_type ==  Number)
+console.log('! =', typeof item_6_type);
