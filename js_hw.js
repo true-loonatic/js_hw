a25e113ae6fa1f940975080a7a28dbfbb45bb2e9
true-loonatic <azinland@gmail.com>
2022-06-16 14:24:36 +0300

    Full and clean

--- js_hw.js
+++ js_hw.js
@@ -1,6 +1,3 @@
-// let apples = 10;
-// console.log("apples =", apples);
-
 let item_1 = 5;
 console.log("item 1 =", item_1);
 let item_2 = 3;
@@ -16,11 +13,6 @@ console.log(item_3 * item_4);
 let item_5 = item_3;
 let item_6 = 15;
 let item_6_type = typeof (item_6);
-
-// let.t
-// if (typeof item_6_type ==  Number)
-// console.log('! =', item_6_type); 
-// console.log('!! =', typeof item_6_type);
 console.log('item_6 ==', item_6, 'item_6_type ==', item_6_type);
 
 let item_7 = String(item_6);
