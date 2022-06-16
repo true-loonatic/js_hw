74712fda9acbfef6ee80a8acc11d8f4a3941eae7
true-loonatic <azinland@gmail.com>
2022-06-16 14:52:31 +0300

    and again

--- js_hw.js
+++ js_hw.js
@@ -24,7 +24,7 @@ let age_2 = 18;
 let age_3 = 60;
 
 if (age_1 < age_2){
-    console.log("You don't have access cause your age is" + age_1 + "it's less then");
+    console.log("You don't have access cause your age is" + age_1 + "it's less then" + age_2);
 }else if (age_1 >= age_2 , age_1 < age_3){
     console.log("Welcome !");
 }else if (age_1 > age_3){
@@ -32,3 +32,4 @@ if (age_1 < age_2){
 }else{
     console.log("Technical work");
 };
+
